935818669e4122155b6d85729f2ce70356e9fcce
karolkorzen <karol.korzen@icloud.com>
2022-01-19 21:00:29 +0100

    Initial Commit

---
+++ RandomUserMe API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUserMe API
+//
+//  Created by Karol Korzeń on 19/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
